9e30613bae06769ce75278676eb4fcf38e8ea84a
Kaihan Wu <kaihanwu@cs.washington.edu>
2026-05-24 22:33:29 -0700

    Add basic frontend for combining files

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, UploadFile, File
 import pandas as pd
 from io import BytesIO
 from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
